6ab92fbb7b523e500322ce6f0f613eceac362443
shehab <ashrafshehab377@gmail.com>
2025-03-18 17:46:29 +0200

    adding BoxInfo Class to dataloader

--- data_utils/dataloader.py
+++ data_utils/dataloader.py
@@ -4,6 +4,24 @@ import torch
 from pathlib import Path
 import pickle
 from typing import List, Dict, Optional, Tuple, Union
+import sys
+
+class BoxInfo:
+    def __init__(self, line):
+        words = line.split()
+        self.category = words.pop()
+        words = [int(string) for string in words]
+        self.player_ID = words[0]
+        del words[0]
+
+        x1, y1, x2, y2, frame_ID, lost, grouping, generated = words
+        self.box = x1, y1, x2, y2
+        self.frame_ID = frame_ID
+        self.lost = lost
+        self.grouping = grouping
+        self.generated = generated
+sys.modules['boxinfo'] = sys.modules[__name__]
+
 
 class Group_Activity_Recognition_Dataset(Dataset):
 
