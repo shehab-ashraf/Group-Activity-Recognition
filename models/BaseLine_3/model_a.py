4cc2029ec87fef384a0aaaaf41cc194ab89acb83
shehab <ashrafshehab377@gmail.com>
2025-03-22 12:00:33 +0200

    update model_a in BaseLine_3

--- models/BaseLine_3/model_a.py
+++ models/BaseLine_3/model_a.py
@@ -4,11 +4,11 @@ the resnet model is fine-tuned to recognize person-level actions
 import torch.nn as nn
 import torchvision.models as models
 
-class Classifer(nn.Module):
+class PlayerActivityClassifier(nn.Module):
     def __init__(self, num_classes):
-        super(Classifer, self).__init__()
+        super(PlayerActivityClassifier, self).__init__()
         
-        self.resnet50 = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+        self.resnet50 = models.resnet50(pretrained=True)
         self.resnet50.fc = nn.Linear(in_features=self.resnet50.fc.in_features, out_features=num_classes)
     
     def forward(self, x):
