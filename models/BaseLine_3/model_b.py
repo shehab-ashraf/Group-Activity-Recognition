b8bb152e752e6c2c49423080feec584e56315d9c
shehab <ashrafshehab377@gmail.com>
2025-03-24 11:21:06 +0200

    update model_b in BaseLine_3

--- models/BaseLine_3/model_b.py
+++ models/BaseLine_3/model_b.py
@@ -5,7 +5,6 @@ class GroupActivityClassifier(nn.Module):
     def __init__(self, model):
         super(GroupActivityClassifier, self).__init__()
 
-        # Feature Extractor: ResNet50 (excluding last FC layer)
         self.feature_extractor = torch.nn.Sequential(*list(model.resnet50.children())[:-1])
         self.feature_extractor.requires_grad_(False) 
 
@@ -15,24 +14,24 @@ class GroupActivityClassifier(nn.Module):
             nn.Linear(2048, 1024),
             nn.BatchNorm1d(1024),
             nn.ReLU(),
-            nn.Dropout(p=0.3),
-            nn.Linear(1024, 512),
-            nn.BatchNorm1d(512),
-            nn.ReLU(),
             nn.Dropout(p=0.5),
             nn.Linear(512, 8)
         )
 
     def forward(self, x):
+        # (batch, bbox, C, H, W) --> (batch * bbox, C, H, W)
         batch, bbox, C, H, W = x.size()
-        x = x.view(batch * bbox, C, H, W)  # Flatten batch & bbox for ResNet
+        x = x.view(batch * bbox, C, H, W)
 
-        x = self.feature_extractor(x)  # Extract features -> (batch * bbox, 2048, 1, 1)
-        x = x.view(batch, bbox, -1)  # Reshape to (batch, bbox, 2048)
+        # (batch * bbox, C, H, W) --> (batch, bbox, 2048)
+        x = self.feature_extractor(x)
+        x = x.view(batch, bbox, -1) 
 
-        x = self.bbox_pool(x.permute(0, 2, 1))  # (batch, 2048, 1)
-        x = x.squeeze(-1)  # (batch, 2048)
+        # (batch, bbox, 2048) --> (batch, 2048)
+        x = self.bbox_pool(x.permute(0, 2, 1)) 
+        x = x.squeeze(-1)
 
+        # (batch, 2048) --> (batch, 8)
         x = self.classifier(x)
 
         return x
