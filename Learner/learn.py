7b843abbae11157de55f0fa51cd8c2917210b824
shehab <ashrafshehab377@gmail.com>
2025-04-07 02:32:36 +0200

    Refactor callbacks with consistent ordering and clean structure

--- Learner/learn.py
+++ Learner/learn.py
@@ -1,7 +1,7 @@
 __all__ = ['CancelFitException', 'CancelBatchException', 'CancelEpochException', 
            'Learner', 'run_cbs', 'to_cpu', 'MetricsCB', 'DeviceCB', 'TrainCB', 
            'ProgressCB', 'with_cbs', 'LRFinderCB', 'lr_find', 'Callback', 
-           'BatchSchedCB', 'EpochSchedCB', 'ConfusionMatrixCB'
+           'BatchSchedCB', 'EpochSchedCB', 'ConfusionMatrixCB', 'OptimizerCB' 
           ]
 
 
@@ -20,24 +20,6 @@ from torch.optim import lr_scheduler
 import seaborn as sns
 import numpy as np
 
-class CancelFitException(Exception): pass
-class CancelBatchException(Exception): pass
-class CancelEpochException(Exception): pass
-
-def_device = 'mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu'
-def to_device(x, device=def_device):
-    if isinstance(x, torch.Tensor): return x.to(device)
-    if isinstance(x, Mapping): return {k:v.to(device) for k,v in x.items()}
-    return type(x)(to_device(o, device) for o in x)
-
-class Callback(): order = 0
-
-def run_cbs(cbs, method_nm, learn=None):
-    for cb in sorted(cbs, key=attrgetter('order')):
-        method = getattr(cb, method_nm, None)
-        if method is not None: method(learn)
-
-
 def to_cpu(x):
     if isinstance(x, Mapping): return {k:to_cpu(v) for k,v in x.items()}
     if isinstance(x, list): return [to_cpu(o) for o in x]
@@ -45,38 +27,35 @@ def to_cpu(x):
     res = x.detach().cpu()
     return res.float() if res.dtype==torch.float16 else res
 
+def_device = 'mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu'
+def to_device(x, device=def_device):
+    if isinstance(x, torch.Tensor): return x.to(device)
+    if isinstance(x, Mapping): return {k:v.to(device) for k,v in x.items()}
+    return type(x)(to_device(o, device) for o in x)
 
-class MetricsCB(Callback):
-    def __init__(self, *ms, **metrics):
-        for o in ms: metrics[type(o).__name__] = o
-        self.metrics = metrics
-        self.all_metrics = copy(metrics)
-        self.all_metrics['loss'] = self.loss = Mean()
 
-    def _log(self, d): print(d)
-    def before_fit(self, learn): learn.metrics = self
-    def before_epoch(self, learn): [o.reset() for o in self.all_metrics.values()]
+class CancelFitException(Exception): pass
+class CancelBatchException(Exception): pass
+class CancelEpochException(Exception): pass
 
-    def after_epoch(self, learn):
-        log = {k:f'{v.compute():.3f}' for k,v in self.all_metrics.items()}
-        log['epoch'] = learn.epoch
-        log['train'] = 'train' if learn.model.training else 'eval'
-        self._log(log)
+class Callback(): order = 0
 
-    def after_batch(self, learn):
-        x,y,*_ = to_cpu(learn.batch)
-        for m in self.metrics.values(): m.update(to_cpu(learn.preds), y)
-        self.loss.update(to_cpu(learn.loss), weight=len(x))
-
+class OptimizerCB(Callback):
+    order = 0 
+    def __init__(self, opt_func=partial(optim.SGD, momentum=0.9)):
+        self.opt_func = opt_func
+    def before_fit(self, learn):
+        learn.opt = self.opt_func(learn.model.parameters())
 
 class DeviceCB(Callback):
+    order = OptimizerCB.order + 1
     def __init__(self, device=def_device): fc.store_attr()
     def before_fit(self, learn):
         if hasattr(learn.model, 'to'): learn.model.to(self.device)
     def before_batch(self, learn): learn.batch = to_device(learn.batch, device=self.device)
 
-
 class TrainCB(Callback):
+    order = DeviceCB.order + 1
     def __init__(self, n_inp=1): self.n_inp = n_inp
     def predict(self, learn): learn.preds = learn.model(*learn.batch[:self.n_inp])
     def get_loss(self, learn): learn.loss = learn.loss_func(learn.preds, *learn.batch[self.n_inp:])
@@ -84,9 +63,28 @@ class TrainCB(Callback):
     def step(self, learn): learn.opt.step()
     def zero_grad(self, learn): learn.opt.zero_grad()
 
+class MetricsCB(Callback):
+    order = TrainCB.order + 1
+    def __init__(self, *ms, **metrics):
+        for o in ms: metrics[type(o).__name__] = o
+        self.metrics = metrics
+        self.all_metrics = copy(metrics)
+        self.all_metrics['loss'] = self.loss = Mean()
+    def _log(self, d): print(d)
+    def before_fit(self, learn): learn.metrics = self
+    def before_epoch(self, learn): [o.reset() for o in self.all_metrics.values()]
+    def after_epoch(self, learn):
+        log = {k:f'{v.compute():.3f}' for k,v in self.all_metrics.items()}
+        log['epoch'] = learn.epoch
+        log['train'] = 'train' if learn.model.training else 'eval'
+        self._log(log)
+    def after_batch(self, learn):
+        x,y,*_ = to_cpu(learn.batch)
+        for m in self.metrics.values(): m.update(to_cpu(learn.preds), y)
+        self.loss.update(to_cpu(learn.loss), weight=len(x))
 
 class ProgressCB(Callback):
-    order = MetricsCB.order+1
+    order = MetricsCB.order + 1
     def __init__(self, plot=False): self.plot = plot
     def before_fit(self, learn):
         learn.epochs = self.mbar = master_bar(learn.epochs)
@@ -94,49 +92,62 @@ class ProgressCB(Callback):
         if hasattr(learn, 'metrics'): learn.metrics._log = self._log
         self.losses = []
         self.val_losses = []
-
     def _log(self, d):
         if self.first:
             self.mbar.write(list(d), table=True)
             self.first = False
         self.mbar.write(list(d.values()), table=True)
-
     def before_epoch(self, learn): learn.dl = progress_bar(learn.dl, leave=False, parent=self.mbar)
     def after_batch(self, learn):
         learn.dl.comment = f'{learn.loss:.3f}'
         if self.plot and hasattr(learn, 'metrics') and learn.training:
             self.losses.append(learn.loss.item())
-            if self.val_losses: self.mbar.update_graph([[fc.L.range(self.losses), self.losses],[fc.L.range(learn.epoch).map(lambda x: (x+1)*len(learn.dls.train)), self.val_losses]])
-    
+            if self.val_losses: self.mbar.update_graph([
+                [fc.L.range(self.losses), self.losses],
+                [fc.L.range(learn.epoch).map(lambda x: (x+1)*len(learn.dls.train)), self.val_losses]
+            ])
     def after_epoch(self, learn): 
         if not learn.training:
             if self.plot and hasattr(learn, 'metrics'): 
                 self.val_losses.append(learn.metrics.all_metrics['loss'].compute())
-                self.mbar.update_graph([[fc.L.range(self.losses), self.losses],[fc.L.range(learn.epoch+1).map(lambda x: (x+1)*len(learn.dls.train)), self.val_losses]])
+                self.mbar.update_graph([
+                    [fc.L.range(self.losses), self.losses],
+                    [fc.L.range(learn.epoch+1).map(lambda x: (x+1)*len(learn.dls.train)), self.val_losses]
+                ])
 
+class BaseSchedCB(Callback):
+    order = TrainCB.order + 2 
+    def __init__(self, sched): self.sched = sched
+    def before_fit(self, learn): self.schedo = self.sched(learn.opt)
+    def _step(self, learn): 
+        if learn.training: self.schedo.step()
+
+class BatchSchedCB(BaseSchedCB):
+    def after_batch(self, learn): self._step(learn)
+
+class EpochSchedCB(BaseSchedCB):
+    order = ProgressCB.order + 1
+    def after_epoch(self, learn): self._step(learn)
 
 class ConfusionMatrixCB(Callback):
+    order = EpochSchedCB.order + 1
     def __init__(self, class_names):
         self.class_names = class_names
         self.metric = MulticlassConfusionMatrix(num_classes=len(class_names))
-    
     def before_epoch(self, learn):
         if not learn.training:
             self.metric.reset()
-    
     def after_batch(self, learn):
         if not learn.training:
             preds = to_cpu(learn.preds).argmax(dim=1)
             targets = to_cpu(learn.batch[1])
             self.metric.update(preds, targets)
-    
     def after_epoch(self, learn):
         if not learn.training:
             cm = self.metric.compute().numpy()
             row_sums = cm.sum(axis=1, keepdims=True)
             row_sums[row_sums == 0] = 1
             cm_percentage = cm / row_sums * 100
-            
             plt.figure(figsize=(6,6))
             sns.heatmap(cm_percentage, annot=True, fmt='.1f', cmap='Greys', cbar=False,
                         xticklabels=self.class_names, yticklabels=self.class_names)
@@ -145,19 +156,30 @@ class ConfusionMatrixCB(Callback):
             plt.title("Confusion Matrix (%)")
             plt.show()
 
+class LRFinderCB(Callback):
+    order = BatchSchedCB.order + 1
+    def __init__(self, gamma=1.3, max_mult=3): fc.store_attr()
+    def before_fit(self, learn):
+        self.sched = ExponentialLR(learn.opt, self.gamma)
+        self.lrs,self.losses = [],[]
+        self.min = math.inf
+    def after_batch(self, learn):
+        if not learn.training: raise CancelEpochException()
+        self.lrs.append(learn.opt.param_groups[0]['lr'])
+        loss = to_cpu(learn.loss)
+        self.losses.append(loss)
+        if loss < self.min: self.min = loss
+        if math.isnan(loss) or (loss > self.min*self.max_mult):
+            raise CancelFitException()
+        self.sched.step()
+    def cleanup_fit(self, learn):
+        plt.plot(self.lrs, self.losses)
+        plt.xscale('log')
 
-class BaseSchedCB(Callback):
-    def __init__(self, sched): self.sched = sched
-    def before_fit(self, learn): self.schedo = self.sched(learn.opt)
-    def _step(self, learn):
-        if (learn.training): self.schedo.step()
-class BatchSchedCB(BaseSchedCB):
-    def after_batch(self, learn): self._step(learn)
-class EpochSchedCB(BaseSchedCB):
-    order = ProgressCB.order+1
-    def after_epoch(self, learn): self._step(learn)
-
-
+def run_cbs(cbs, method_nm, learn=None):
+    for cb in sorted(cbs, key=attrgetter('order')):
+        method = getattr(cb, method_nm, None)
+        if method is not None: method(learn)
 
 class with_cbs:
     def __init__(self, nm): self.nm = nm
@@ -173,7 +195,7 @@ class with_cbs:
 
 
 class Learner():
-    def __init__(self, model, dls=(0,), loss_func=F.mse_loss, lr=0.1, cbs=None, opt_func=optim.Adam):
+    def __init__(self, model, dls=(0,), loss_func=F.mse_loss, lr=0.1, cbs=None):
         cbs = fc.L(cbs)
         fc.store_attr()
 
@@ -212,7 +234,6 @@ class Learner():
             self.n_epochs = n_epochs
             self.epochs = range(n_epochs)
             if lr is None: lr = self.lr
-            if self.opt_func: self.opt = self.opt_func(self.model.parameters(), lr)
             self._fit(train, valid)
         finally:
             for cb in cbs: self.cbs.remove(cb)
@@ -226,29 +247,6 @@ class Learner():
     @property
     def training(self): return self.model.training
 
-
-class LRFinderCB(Callback):
-    def __init__(self, gamma=1.3, max_mult=3): fc.store_attr()
-    
-    def before_fit(self, learn):
-        self.sched = ExponentialLR(learn.opt, self.gamma)
-        self.lrs,self.losses = [],[]
-        self.min = math.inf
-
-    def after_batch(self, learn):
-        if not learn.training: raise CancelEpochException()
-        self.lrs.append(learn.opt.param_groups[0]['lr'])
-        loss = to_cpu(learn.loss)
-        self.losses.append(loss)
-        if loss < self.min: self.min = loss
-        if math.isnan(loss) or (loss > self.min*self.max_mult):
-            raise CancelFitException()
-        self.sched.step()
-
-    def cleanup_fit(self, learn):
-        plt.plot(self.lrs, self.losses)
-        plt.xscale('log')
-
 @fc.patch
 def lr_find(self:Learner, gamma=1.3, max_mult=3, start_lr=1e-5, max_epochs=10):
-    self.fit(max_epochs, lr=start_lr, cbs=LRFinderCB(gamma=gamma, max_mult=max_mult))
+    self.fit(max_epochs, lr=start_lr, cbs=[LRFinderCB(gamma=gamma, max_mult=max_mult)])
