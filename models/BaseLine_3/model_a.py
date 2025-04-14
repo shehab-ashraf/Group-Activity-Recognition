cf2a7c91776ab15ea7734605c60bd40e1f109133
shehab <ashrafshehab377@gmail.com>
2025-04-14 11:27:20 +0200

    Replace default ResNet50 FC layer with custom sequential block, Include LayerNorm, GELU activation, Dropout and Implement Xavier initialization for linear layers

--- models/BaseLine_3/model_a.py
+++ models/BaseLine_3/model_a.py
@@ -1,15 +1,33 @@
-"""
-the resnet model is fine-tuned to recognize person-level actions
-"""
 import torch.nn as nn
 import torchvision.models as models
+import torch.nn.init as init
 
-class PlayerActivityClassifier(nn.Module):
-    def __init__(self, num_classes):
-        super(PlayerActivityClassifier, self).__init__()
+class Player_Activity_Classifier(nn.Module):
+    def __init__(self, num_classes=9):
+        super(Player_Activity_Classifier, self).__init__()
+        self.resnet50 = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+        self.resnet50.fc = nn.Sequential(
+            nn.Linear(self.resnet50.fc.in_features, 1024),
+            nn.LayerNorm(1024),
+            nn.GELU(),
+            nn.Dropout(0.3),
+            nn.Linear(1024, num_classes)
+        )
         
-        self.resnet50 = models.resnet50(pretrained=True)
-        self.resnet50.fc = nn.Linear(in_features=self.resnet50.fc.in_features, out_features=num_classes)
-    
+        # Initialize weights
+        self._initialize_weights()
+
+
+    def _initialize_weights(self):
+        for m in self.resnet50.fc.modules():
+            if isinstance(m, nn.Linear):
+                init.xavier_normal_(m.weight, gain=1.0)
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+            elif isinstance(m, nn.LayerNorm):
+                init.constant_(m.weight, 1)
+                init.constant_(m.bias, 0)
+            
     def forward(self, x):
+        # Input shape: (batch_size, C, H, W)
         return self.resnet50(x)
