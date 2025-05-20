5196a44b062c18c580a01160c35ebd896db1579e
shehab <ashrafshehab377@gmail.com>
2025-05-20 16:26:43 +0300

    Adding show_frame function

--- helper_utils/helper.py
+++ helper_utils/helper.py
@@ -1,9 +1,20 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+import torchvision.transforms.functional
 
 
 
 def show_image(img):
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+def show_frame(frames):
+    plt.figure(figsize=(18, 3))
+    for i in range(len(frames)):
+        plt.subplot(1, len(frames), i + 1)
+        plt.imshow(torchvision.transforms.functional.to_pil_image(frames[i]))
+        plt.axis('off')
+    plt.tight_layout()
     plt.show()
