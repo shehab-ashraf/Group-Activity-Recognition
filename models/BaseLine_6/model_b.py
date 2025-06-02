0d1a2cf2d2e59cc38cc9f14b709436fe43d899ff
shehab <ashrafshehab377@gmail.com>
2025-06-02 14:16:47 +0300

    Update Group_Activity_Classifier model for Baseline 6

--- models/BaseLine_6/model_b.py
+++ models/BaseLine_6/model_b.py
@@ -3,22 +3,26 @@ import torch.nn as nn
 import torchvision.models as models
 
 class Group_Activity_Classifier(nn.Module):
-    def __init__(self, Person_Activity_Model, hidden_size=512, num_classes=8):
+    def __init__(self, Player_Activity_Model, hidden_size=512, num_classes=8):
         super(Group_Activity_Classifier, self).__init__()
 
-        self.feature_extraction = nn.Sequential(*list(Person_Activity_Model.resnet50.children())[:-1])
+        self.resnet50 = nn.Sequential(*list(Player_Activity_Model.resnet50.children())[:-1])
+
+        for param in self.resnet50.parameters():
+            param.requires_grad = False
 
         self.pool = nn.AdaptiveMaxPool2d((1, 2048))
 
         self.lstm = nn.LSTM(
-            input_size=2048, 
-            hidden_size=hidden_size, 
-            num_layers=1, 
-            batch_first=True)
+            input_size=2048,
+            hidden_size=hidden_size,
+            num_layers=1,
+            batch_first=True
+        )
 
         self.fc = nn.Sequential(
             nn.Linear(hidden_size, 256),
-            nn.BatchNorm1d(256),
+            nn.LayerNorm(256),
             nn.ReLU(),
             nn.Dropout(0.3),
 
@@ -26,13 +30,13 @@ class Group_Activity_Classifier(nn.Module):
         )
     
     def forward(self, x):
-        # Innput shape: (batch, seq, num_players, C, H, W)  
-        batch, seq, num_players, C, H, W = x.size()
-        # (batch, seq, num_players, C, H, W) --> (batch*seq*num_players, C, H, W)
-        x = x.view(-1, C, H, W)
-        # (batch*seq*num_players, C, H, W) --> (batch*seq*num_players, 2048)
-        x = self.feature_extraction(x)
-        # (batch*seq*num_players, 2048) --> (batch_size, seq, num_players, 2048)
+        # Innput shape: (batch, seq, num_players, C, H, W)
+        batch, seq, num_players, c, h, w = x.size()
+        # (batch, seq, num_players, c, h, w) --> (batch*seq*num_players, c, h, w)
+        x = x.view(-1, c, h, w)
+        # (batch*seq*num_players, c, h, w) --> (batch*seq*num_players, 2048)
+        x = self.resnet50(x)
+        # (batch*seq*num_players, 2048) --> (batch, seq, num_players, 2048)
         x = x.view(batch, seq, num_players, -1)
         # (batch, seq, num_players, 2048) --> (batch_size, seq, 2048)
         x = self.pool(x)
