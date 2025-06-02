9063194b775c211d45e5e2e2edea21f1dbb1469b
shehab <ashrafshehab377@gmail.com>
2025-06-02 14:01:17 +0300

    Update Group_Activity_Classifier model for Baseline 3

--- models/BaseLine_3/model_b.py
+++ models/BaseLine_3/model_b.py
@@ -1,37 +1,37 @@
 import torch
 import torch.nn as nn
 
-class GroupActivityClassifier(nn.Module):
-    def __init__(self, model):
-        super(GroupActivityClassifier, self).__init__()
+class Group_Activity_Classifier(nn.Module):
+    def __init__(self, Player_Activity_Model, num_classes=8):
+        super(Group_Activity_Classifier, self).__init__()
 
-        self.feature_extractor = torch.nn.Sequential(*list(model.resnet50.children())[:-1])
-        self.feature_extractor.requires_grad_(False) 
+        self.resnet50 = torch.nn.Sequential(*list(Player_Activity_Model.resnet50.children())[:-1])
+
+        for param in self.resnet50.parameters():
+            param.requires_grad = False
+        
+        self.feature_extraction.requires_grad_(False)
 
         self.pool = nn.AdaptiveMaxPool2d((1, 2048))
 
         self.fc = nn.Sequential(
             nn.Linear(2048, 1024),
-            nn.BatchNorm1d(1024),
+            nn.LayerNorm(1024),
             nn.ReLU(),
             nn.Dropout(0.5),
-            nn.Linear(1024, 8)
+            nn.Linear(1024, num_classes)
         )
 
     def forward(self, x):
         # (batch, bbox, C, H, W) --> (batch * bbox, C, H, W)
         batch, bbox, C, H, W = x.size()
         x = x.view(batch * bbox, C, H, W)
-
         # (batch * bbox, C, H, W) --> (batch, bbox, 2048)
-        x = self.feature_extractor(x)
-        x = x.view(batch, bbox, -1) 
-
+        x = self.feature_extraction(x)
+        x = x.view(batch, bbox, -1)
         # (batch, bbox, 2048) --> (batch, 2048)
         x = self.pool(x) 
         x = x.squeeze()
-
         # (batch, 2048) --> (batch, 8)
         x = self.fc(x)
-
         return x
