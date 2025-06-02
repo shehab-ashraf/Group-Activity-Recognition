326bbcd9d1f7bb95d040988412848b47b59a1c45
shehab <ashrafshehab377@gmail.com>
2025-06-02 11:56:34 +0300

    Update Group_Activity_Classifier model for Baseline 1

--- models/BaseLine_1/model.py
+++ models/BaseLine_1/model.py
@@ -2,16 +2,13 @@ import torch.nn as nn
 from torchvision import models
 from typing import Optional
 
-class GroupActivityClassifier(nn.Module):
-    def __init__(self, num_classes: int, freeze_backbone: Optional[bool] = False):
-        super(GroupActivityClassifier, self).__init__()
-        self.resnet50 = models.resnet50(pretrained=True)
+class Group_Activity_Classifier(nn.Module):
+    def __init__(self, num_classes):
+        super(Group_Activity_Classifier, self).__init__()
 
-        if freeze_backbone:
-            for param in self.resnet50.parameters():
-                param.requires_grad = False
-        
+        self.resnet50 = models.resnet50(weights=models.ResNet50_Weights.DEFAULT) 
         self.resnet50.fc = nn.Linear(self.resnet50.fc.in_features, num_classes)
 
     def forward(self, x):
+        # Input_Shape: (Batch_Size, 3, 224, 224)
         return self.resnet50(x)
