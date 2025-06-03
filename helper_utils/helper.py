6f7e4654fbd58739f8a4bb0c09928f91ed681dd1
shehab <ashrafshehab377@gmail.com>
2025-06-03 11:21:20 +0300

    Add show_images() function to visualize a batch of images

--- helper_utils/helper.py
+++ helper_utils/helper.py
@@ -1,20 +1,23 @@
 import numpy as np
+import torch
+import torchvision
 import matplotlib.pyplot as plt
-import torchvision.transforms.functional
-
-
 
 def show_image(img):
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.axis('off')
+    plt.show()
+
+
+def show_images(imgs):
+    grid_img = torchvision.utils.make_grid(imgs, nrow=len(imgs))
+    npimg = grid_img.numpy()
+    plt.figure(figsize=(len(imgs)*2, 2))
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.axis('off')
     plt.show()
 
 
 def show_frame(frames):
-    plt.figure(figsize=(18, 3))
-    for i in range(len(frames)):
-        plt.subplot(1, len(frames), i + 1)
-        plt.imshow(torchvision.transforms.functional.to_pil_image(frames[i]))
-        plt.axis('off')
-    plt.tight_layout()
-    plt.show()
+    show_images(frames)
